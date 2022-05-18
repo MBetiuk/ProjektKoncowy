521963dde8236122ee9bceedf5c5044f28b03ace
MBetiuk <mbetiuk1@gmail.com>
2022-05-18 21:41:26 +0200

    Konfiguracja springboot i utworzenie bazy danych

---
+++ src/main/java/pl/betiuk/projektkoncowy/ProjektKoncowyApplication.java
@@ -0,0 +1,13 @@
+package pl.betiuk.projektkoncowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKoncowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektKoncowyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/betiuk/projektkoncowy/ProjektKoncowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.betiuk.projektkoncowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKoncowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
